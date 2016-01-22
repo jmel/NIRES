1eddd975f455f5c0ea7cb71a992eb9c594c13074
Jason Melbourne <jason.melbourne@gmail.com>
2016-01-22 10:33:28 -0800

    handling old and new imager names

--- nires/imDisplay.py
+++ nires/imDisplay.py
@@ -5,12 +5,12 @@ import os
 import globals
 
 def readImage(fn):
-	try:
-		a = pf.open(fn)[0].data
-	except:
-		print "Could not open %s" % fn
-	
-	return a
+    try:
+        a = pf.open(fn)[0].data
+    except:
+        print "Could not open %s" % fn
+    
+    return a
 
 def is_number(s):
     try:
@@ -20,47 +20,56 @@ def is_number(s):
         return False
 
 def getMostRecentFile(dir, prefix='s', suffix='.fits'):
-	files = sorted([ f for f in os.listdir(dir) if f.startswith(prefix) and f.endswith(suffix)])
-	return files[-1]
+    files = sorted([ f for f in os.listdir(dir) if f.startswith(prefix) and f.endswith(suffix)])
+    return files[-1]
 
-def nameResolve(indexString,prefix): #removed the default setting of s*
+def nameResolve(indexString, prefix): #removed the default setting of s*
 
-	# check if only want most recent file indcated by indexString='c'
-	if indexString == 'c':
-		try:
-			return getMostRecentFile('.',prefix,'fits')
-		except:
-			return ''
-	# Check for length of input string to determine if you need to construct the name
+    # check if only want most recent file indcated by indexString='c'
+    if indexString == 'c':
+        try:
+            return getMostRecentFile('.', prefix, 'fits')
+        except:
+            return ''
+    # Check for length of input string to determine if you need to construct the name
 
-	if is_number(indexString):
-		indexStringLen=len(indexString)
-	
-		if indexStringLen < 4:
-			num=indexString
-			for i in range(4-indexStringLen):
-				num='0'+num
-			name=prefix+num+'.fits'	
-		else:
-			name=prefix+indexString+'.fits'
-			
+    if is_number(indexString):
+        indexStringLen = len(indexString)
+    
+        if indexStringLen < 4:
+            num = indexString
+            for i in range(4 - indexStringLen):
+                num = '0' + num
+            name = prefix + num + '.fits'    
+        else:
+            name = prefix + indexString + '.fits'
+            
+        try:
+            full_name = glob.glob(globals.datapath + name)[0] #made the path absolute, used the path constant from globals
+        except: 
+        	# handle old version of the images 
+            if name[0] == 'v':
+                try:
+                    name = 'i' + name[1:]
+                    full_name = glob.glob(globals.datapath + name)[0]
+                except:
+                    print 'Image number out of range' 
+                    full_name = ''
+            else:
+                print 'Image number out of range' 
+                full_name = ''
 
-		try:
-                        name=glob.glob(globals.datapath + name)[0] #made the path absolute, used the path constant from globals
-			
-		except: 
-			print 'Image number out of range' 
-			name=''
+        return full_name
 
-		return name
-
-	return indexString
+    return indexString
 
 def returnInst(instString): #removed the default setting of s
-	if instString == 'v':
-		title='Viewer'
-		prefix='i*'	
-	elif instString == 's':
-		title='Spectrograph'
-		prefix='s*'
-	return title,prefix
+    if instString == 'v':
+        title = 'Viewer'
+        prefix = 'v*'    
+    elif instString == 's':
+        title = 'Spectrograph'
+        prefix = 's*'
+    else :
+        print 'Please specify v for Viewer or s for Spectrograph'
+    return title,prefix
